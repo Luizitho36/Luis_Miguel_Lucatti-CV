410670e2edb2a2e7246917d026a9bdb7d0fec010
Luizitho36 <luizitho36@hotmail.com>
2024-08-18 12:41:19 -0300

    Agrego parrafos y un paquete json con mis datos

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+// import {Data} from './data/data.json';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>Hello World</h1>
+
+      <p>
+        MIS DATOS
+      </p>
+
+      <p>
+        MI NIVEL SECUNDARIO
+      </p>
+
+      <p>
+        IDIOMAS
+      </p>
+
+      <p>
+        MI EXPERIENCIA LABORAL
+      </p>
+
+      <p>
+        MIS FORMACIONES ADICIONALES E INTERESES
+      </p>
+
+      <p>
+        MIS CONOCIMIENTOS DE PROGRAMACIÓN
+      </p>
+
+      <p>
+        MIS REDES
+      </p>
+
+      <p>
+        OBJETIVO
+      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
