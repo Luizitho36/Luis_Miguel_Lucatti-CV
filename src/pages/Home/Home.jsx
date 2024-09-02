9356d8a50b5bf2656deb2a0cb6363f98e7ec3345
Luizitho36 <luizitho36@hotmail.com>
2024-09-02 14:19:52 -0300

    presentación_cambios

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,11 +7,10 @@ function Home() {
       <h2 className="párrafo2">¡HOLA, MI NOMBRE ES <b className="título">LUIS MIGUEL LUCATTI</b>!</h2>
       <section className="sidebar"><img className="imageOfMe" src={ViewImage}></img></section>
       <b id="about_me" className="párrafo1">
-        y busco desarrollarme profesionalmente en un ámbito laboral que me permita, mediante retos,
+        y busco desarrollarme en un ámbito laboral que me permita, mediante desafíos,
         aplicar los conocimientos adquiridos con la mayor eficiencia y, así mismo, poder
         superarme mediante los nuevos. Estoy dispuesto a ofrecer toda mi capacidad
-        intelectual, física y moral, como así también, creatividad e imaginación,
-        para el máximo crecimiento profesional.
+        intelectual, mi creatividad e imaginación, para el máximo crecimiento profesional.
       </b>
     </div>
   );
